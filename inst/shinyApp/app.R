43c1f368e7ad0109f757506ed7b3930e1f37015e
Evan <evan.ansell@gmail.com>
2020-02-10 16:33:04 -0500

    Adding shinyApp to floodnetRfa
    Adding files from seperate shiny app repo to the inst/shinyApp directory -- will test to see how installation of package impacted

---
+++ inst/shinyApp/app.R
@@ -0,0 +1,227 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(CSHShydRology)
+
+source('./Functions/inplaceAmax.R')
+source('./Functions/inplacePot.R')
+source('./Functions/rfaAmax.R')
+source('./Functions/rfaPot.R')
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(theme = "mystyle.css",
+                
+    # Application title
+    titlePanel("Floodnet 1-6 Toolbox"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            tags$head(tags$style(type="text/css", 
+               "#loadmessage {
+               position: fixed;
+               top: 0px;
+               left: 0px;
+               width: 100%;
+               padding: 5px 0px 5px 0px;
+               text-align: center;
+               font-weight: bold;
+               font-size: 100%;
+               color: #000000;
+               background-color: #CCFF66;
+               z-index: 105;
+             }
+            ")),
+            selectInput("station", label = h3("Target Site"), 
+                          choices = list("01AF009" = "01AF009",
+                                         "01AD003" = '01AD003',
+                                          "01AF007" = '01AF007',
+                                          "01AJ003" = '01AJ003',
+                                          "01AJ004" = '01AJ004',
+                                          "01AJ010" = '01AJ010'
+                          ), selected = "01AF009"),
+            textInput("periodString", label = h3("Return Period"), value = "10,100"),
+            selectInput("method", label = h3("Method"),
+                        choices = list("AMAX" = "amax",
+                                       "POT" = "pot",
+                                       "RFA AMAX" = "rfaAmax",
+                                       "RFA POT" = "rfaPot"
+                        ), selected = "amax"),
+            
+            ## The option to select the distribution method is only available for AMAX
+            ## Therefore this selectInput is hidden for POT
+            conditionalPanel(condition = "input.method == 'amax' || input.method == 'rfaAmax'",
+                selectInput("distr", label = h3("Distribution"), 
+                        choices = list("Default" =  "Default",
+                                       "gev" = "gev",
+                                       "glo" = "glo",
+                                       "gno" = "gno",
+                                       "pe3" = "pe3"
+                        ), selected = "Default"),
+            ),
+            
+            actionButton(inputId = 'update', label = 'Update'),
+            # textOutput("loading", inline = TRUE)
+            
+            # loading message and css taken from user1603038's post at https://stackoverflow.com/questions/17325521/r-shiny-display-loading-message-while-function-is-running 
+            # will be customized in the future - a placeholder for now
+            conditionalPanel(condition="$('html').hasClass('shiny-busy')",
+                             tags$div("Loading...",id="loadmessage"))
+        ),
+        
+           
+        mainPanel(
+            
+            tags$h3("Plot"),
+            fluidRow(
+                # imageOutput("loading"),
+                plotOutput("plot")
+            ),
+            tags$h3("Estimated Flood Quantile"),
+            fluidRow(
+                # imageOutput("loading"),
+               tableOutput("table")
+            )
+        )
+        
+        
+    ) 
+    # sidebarLayout(
+    #     sidebarPanel(tags$p("test")),
+    #     mainPanel()
+    # )
+    
+    
+    ## Custom-css method, if we want sidebar split up, designed for landscape, etc.
+    # fluidRow(
+    #     column(4,HTML("<div class='inputBox'>
+    #          "),
+    #            column(6,
+    #                   selectInput("station", label = h3("Select Station"), 
+    #                               choices = list("01AF009" = "01AF009",
+    #                                              "01AD003" = '01AD003',
+    #                                              "01AF007" = '01AF007',
+    #                                              "01AJ003" = '01AJ003',
+    #                                              "01AJ004" = '01AJ004',
+    #                                              "01AJ010" = '01AJ010'
+    #                               ))
+    #                   ),
+    #           column(6,
+    #                  textInput("periodString", label = h3("Return Period"), value = "10,100"),
+    #                  ),
+    #           fluidRow(
+    #               actionButton(inputId = 'update', label = 'Update'),
+    #               HTML("</div>")  
+    #           )
+    #            
+    #           
+    #           
+    #     )
+    #            
+    # ),
+    #     
+    # 
+    #         
+    #         
+    #     # column(6, 
+    #            #inputPanel(# Input station selector
+    #               # fluidRow(
+    # #               column(6,
+    # #                     selectInput("station", label = h3("Select Station"), 
+    # #                                   choices = list("Iroquois River at Moulin Morneault" = "01AF009", 
+    # #                                                  "ST. FRANCIS RIVER AT OUTLET OF GLASIER LAKE" = '01AD003',
+    # #                                                  "GRANDE RIVIERE AT VIOLETTE BRIDGE" = '01AF007',
+    # #                                                  "MEDUXNEKEAG RIVER NEAR BELLEVILLE" = '01AJ003',
+    # #                                                  "BIG PRESQUE ISLE STREAM AT TRACEY MILLS" = '01AJ004',
+    # #                                                  "BECAGUIMEC STREAM AT COLDSTREAM" = '01AJ010'
+    # #                                   ))
+    # #                 ),
+    # #                 column(6,
+    # #                     textInput("periodString", label = h3("Return Period"), value = "Enter period. Eg 10,100...")
+    # #                 ),
+    # #             
+    # #                
+    # #                 actionButton(inputId = 'update', label = 'Update')
+    # #            )
+    # #            
+    # #            )
+    # # ),
+    # #fluidRow(tags$h3("In-place Amax")),
+    # fluidRow(
+    #     headerPanel("In-place Amax"),
+    #     column(width = 6, tableOutput("amaxTable")),
+    #     column(width = 6, plotOutput("amaxPlot"))
+    # ),
+    # fluidRow("In-place POT"),
+    # fluidRow(
+    #     column(width = 6, tableOutput("potTable")),
+    #     column(width = 6, plotOutput("potPlot"))
+    # ),
+    # fluidRow("RFA Amax"),
+    # fluidRow(
+    #     column(width = 6, tableOutput("rfaAmaxTable")),
+    #     column(width = 6, plotOutput("rfaAmaxPlot"))
+    # ),
+    # fluidRow("RFA POT"),
+    # fluidRow(
+    #     column(width = 6, tableOutput("rfaPotTable")),
+    #     column(width = 6, plotOutput("rfaPotPlot"))
+    # )
+    
+    
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    # You can access the value of the widget with input$select, e.g.
+    
+    # observeEvent(input$update, {
+    #     output$loading <- list(
+    #         src = "www/loadingSpinnger.svg",
+    #         contentType = "image/png",
+    #         alt = "Spinner"
+    #     )
+    # })
+    
+    # observeEvent(input$update, {
+    #     output$loading <- renderText("Loading...")
+    # })
+   
+    
+    # Making eventReactive so table/plot updates with button instead of automatically
+    # Storing values in result so each function is only run once
+    result <- eventReactive(input$update, {
+        if(input$method == "amax"){
+            inplaceAmax(input$station, as.integer(unlist(strsplit(input$periodString,','))), input$distr)
+            # output$loading <- NULL
+        } else if(input$method == "pot"){
+            inplacePot(input$station, as.integer(unlist(strsplit(input$periodString,','))))
+            # output$loading <- NULL
+        } else if(input$method == "rfaAmax"){
+            rfaAmax(input$station, as.integer(unlist(strsplit(input$periodString,','))), input$distr)
+            # output$loading <- NULL
+        } else {
+            rfaPot(input$station, as.integer(unlist(strsplit(input$periodString,','))))
+            # output$loading <- NULL
+        }
+        
+    })
+    
+    
+    # output functions to table/plot
+    output$table <- renderTable({ result()$qua })
+    output$plot <- renderPlot( plot(result()$fit) )
+    
+    
+                               
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
