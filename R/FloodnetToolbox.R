cfb953a02ec3a1681364d80c53458516994577d5
Evan <evan.ansell@gmail.com>
2020-02-12 12:01:24 -0500

    Exported function to run GUI
    FloodnetToolbox() should now be a function usable from installation of the package
    Note: the floodnetRfa.Rmd vignette failed to re-build on my end, though nothing was changed. Maybe an issue with my environment?

---
+++ R/FloodnetToolbox.R
@@ -0,0 +1,19 @@
+
+
+#' Run Shiny Toolbox GUI
+#'
+#' A simple function used to run the Shiny GUI from the floodnetRfa package isntallation
+#'
+#' @return None
+#' @export
+#'
+#' @examples
+#' FloodnetToolbox()
+FloodnetToolbox <- function() {
+
+	###################################
+	## Start the shinyApp
+	###################################
+
+	shiny::runApp('inst/shinyApp')
+}
